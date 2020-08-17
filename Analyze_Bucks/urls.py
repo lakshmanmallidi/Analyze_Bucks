4464db899410e947700e7af521cd2c9ee55fe5b9
lakshman.mallidi <lakshman.mallidi@gmail.com>
2020-08-18 01:19:12 +0530

    setup for  allowed host for prod

--- Analyze_Bucks/urls.py
+++ Analyze_Bucks/urls.py
@@ -19,7 +19,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 from django.conf.urls.static import static 
 from django.conf import settings
 
-print(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
 urlpatterns = [
     path('finance/', include('finance.urls')),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
